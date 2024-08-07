f2c00cee7e83676f0f419e94b9539d068b282185
Wissem Sghaier <wissemsghaier2000@gmail.com>
2024-08-07 11:18:00 +0100

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,42 @@ app.use(passport.initialize());
 require("./security/passport")(passport);
 
 // 3- MongoDB connection
-mongoose
-  .connect("mongodb://localhost:27017/authentication", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Failed to connect to MongoDB", error));
+// mongoose
+//   .connect("mongodb://localhost:27017/authentication", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((error) => console.error("Failed to connect to MongoDB", error));
+
+// 3- MongoDB connection
+//const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/authentication";
+// const mongoUri = process.env.MONGO_URI || "mongodb://root:mongopass@mongo:27017/authentication?authSource=admin";
+// const certPath = process.env.MONGO_CERT_PATH || '/app/certs/mongodb-cert.crt';
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   tls: true,
+//   tlsCAFile: certPath, // Chemin vers le certificat CA
+// };
+
+// mongoose
+//   .connect(mongoUri, options)
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((error) => console.error("Failed to connect to MongoDB", error));
+
+
+const mongoUri = process.env.MONGO_URI || "mongodb://root:mongopass@mongo:27017/authentication?authSource=admin";
+
+  mongoose
+    .connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error("Failed to connect to MongoDB", error));
+  
+
 
 // 4- Global error handler
 // app.use((err,res,req,next)=>{
